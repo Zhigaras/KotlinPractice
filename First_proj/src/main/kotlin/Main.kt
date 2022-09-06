810a56ef9aabae17330aa93d53fc6924ee3494e5
Zhigaras Ilya <jigaras@gmail.com>
2022-09-06 21:25:54 +0300

    homworke is done

---
+++ First_proj/src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    println("")
+    // пустая строка в начале
+    println("Hello!")
+    print("My name is Ilya. ")
+    println("I`m 33 y.o.")
+    print("I learned Python few days ago.")
+    //println("I learned Python few days ago.")
+    println("")
+    // пустая строка в конце
+    /* хочу изучить Котлин
+    Было бы здорово */
+}   
