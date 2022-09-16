f11c9d617d469d16cf718154a336b5760af35d2b
Zhigaras Ilya <jigaras@gmail.com>
2022-09-17 02:23:57 +0300

    homework is done

---
+++ 07_ClassesAndObjects/ClassesAndObjects/src/main/kotlin/Channels.kt
@@ -0,0 +1,28 @@
+import kotlin.collections.List
+
+object Channels {
+    internal val allChannels = listOf(
+        "1st channel",
+        "News",
+        "Movies",
+        "24/7",
+        "Sport",
+        "Nature",
+        "National Geographic",
+        "Culture",
+        "Cinema",
+        "TV Series",
+        "Fishing",
+        "Hunting",
+        "Football 24",
+        "Racing",
+        "Formula 1",
+        "Hockey 24"
+    )
+
+    fun getRandomChannels(amountOfChannels: Int): List<String> {
+        val amountOfChannels = minOf(amountOfChannels, allChannels.size)
+        val channelList = allChannels.shuffled().slice(0 until amountOfChannels)
+        return channelList
+    }
+}
